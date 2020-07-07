5d5decef87681fbe1f054a9640f4ab8d88ba4c2e
Ralph L. Rose <rose@waseda.jp>
2020-07-07 22:49:46 +0900

    Updated quiz.js and item.js

--- item.js
+++ item.js
@@ -10,16 +10,98 @@
 const itemTypes = {
     MULTICHOICE_CLOZE: "Multiple choice cloze",
     MULTICHOICE_SYNONYM: "Multiple choice synonym",
-    // REARRANGE: "Rearrange",
+    // MULTICHOICE_REARRANGE: "Multiple choice rearrange",
     // MATCHING: "Matching",
     FREERESPONSE_CLOZE: "Free response cloze"
 }
 
 class Item {
-    type = null;
-    target = null;
+    _type = null;
+    _target = null;
 
     constructor(type) {
-        this.type = type;
+        this._type = type;
     }
+}
+
+class MultichoiceItem extends Item {
+    _options = [];
+
+    constructor(type) {
+        super(type);
+    }
+
+    get options() {
+        return this._options;
+    }
+
+    set options(options) {
+        this._options = options;
+    }
+}
+
+class MultichoiceClozeItem extends MultichoiceItem {
+    _stem = null;
+
+    constructor(type) {
+        super(type);
+    }
+
+    get stem() {
+        return this._stem;
+    }
+
+    set stem(stem) {
+        this._stem = stem;
+    }
+}
+
+class MultichoiceSynonymItem extends MultichoiceItem {
+    _stem = null;
+
+    constructor(type) {
+        super(type);
+    }
+
+    get stem() {
+        return this._stem;
+    }
+
+    set stem(stem) {
+        this._stem = stem;
+    }
+}
+
+class MultichoiceRearrangeItem extends MultichoiceItem {
+    // TODO: Define properties
+
+    constructor(type) {
+        super(type);
+    }
+    // TODO: Define functions
+}
+
+class FreeresponseClozeItem extends Item {
+    _stems = [];
+
+    constructor(type) {
+        super(type);
+    }
+
+    get stems() {
+        return this._stems;
+    }
+
+    set stems(stems) {
+        this._stems = stems;
+    }
+}
+
+class MatchingItem extends Item {
+    // TODO: Define properties
+
+    constructor(type) {
+        super(type);
+    }
+    // TODO: Define functions
 }
--- quiz.js
+++ quiz.js
@@ -1,39 +1,79 @@
-/* Title: quiz.js
- * Author: Ralph L. Rose
- * E-mail address: rose@waseda.jp
- * Description: Definition file for Quiz object
- * License: The MIT License
- */
+/**
+* @file Definition file for Quiz object
+* @author Ralph L. Rose <rose@waseda.jp>
+* @copyright Ralph L. Rose 2020
+* @license The MIT License
+*/
 
 "use strict";
 
+/**
+* This defines the Quiz class which is the main object
+* that EWE works on and creates in one run.
+*/
 class Quiz {
-    structure = null;
-    itemGroups = [];
-    title = ""; // TODO: How to deal with localization of title and instructions?
-    instructions = "";
+    _structure = null;
+    _itemGroups = [];
+    _title = ""; // TODO: How to deal with localization of title and instructions?
+    _instructions = "";
 
     constructor(structure){
         if(!isWellFormedQuizStructure(structure)) {
             throw new Error('Cannot initialize Quiz object: Structure definition is not well-formed.')
         } else {
-            this.structure = structure;
+            this._structure = structure;
         }
     }
 
     get structure() {
-        return this.structure;
+        return this._structure;
     }
 
     set title(title) {
-        this.title = title;
+        this._title = title;
     }
 
     set instructions(instructions) {
-        this.instructions = instructions;
+        this._instructions = instructions;
     }
 }
 
+/**
+* A class which defines a group of like items, forming one
+* part of a complete Quiz.
+*/
+class ItemGroup {
+    _title = null;
+    _itemType = null;
+    _items = [];
+
+    constructor(itemType) {
+        this._itemType = itemType;
+    }
+
+    get title(title) {
+        return this._title;
+    }
+
+    set title(title) {
+        this._title = title;
+    }
+
+    get items() {
+        return this._items;
+    }
+
+    set items(items) {
+        this._items = items;
+    }
+}
+
+/** 
+* A function to evaluate whether a json object is a well-formed
+* expression of the structure of a Quiz.
+* @param {json} structure - A json object that describes the structure of a quiz
+* @return {boolean} If the structure object is well-formed, true. Otherwise, false.
+*/
 function isWellFormedQuizStructure(structure) {
     var groups = structure.split(",");
     groups.forEach(element => {
@@ -50,7 +90,7 @@ function isWellFormedGroupStructure(structure){
         return false;
     }
     var result = false;
-    for (type in itemTypes) {
+    for (type in itemTypes) { // Q: Use eweGlobals instead?
         if (type === strucInfo[0]){
             result = true;
             break;
